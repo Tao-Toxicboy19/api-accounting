f648bb5c086ba2eedf31cf222f634eb462082fb3
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-07 21:20:28 +0700

    [IMP] setup docker compose, connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(`mongodb://localhost:27017`, {
+      user: 'root',
+      pass: 'example',
+      dbName: 'accounting',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
