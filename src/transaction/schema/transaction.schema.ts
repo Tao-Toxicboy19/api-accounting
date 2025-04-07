b09d1d85d6f425363a5bb2183c2433676a1e3b2f
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-07 23:02:14 +0700

    [IMP] check type,audit, transaction res

---
+++ src/transaction/schema/transaction.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TransactionDocument = Transaction & Document;
+
+@Schema({ timestamps: true })
+export class Transaction {
+  @Prop({ required: true, index: true })
+  user: string;
+
+  @Prop({ required: true, enum: ['income', 'expense', 'installment'] })
+  type: 'income' | 'expense' | 'installment';
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  amount: number;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop()
+  category?: string;
+
+  @Prop()
+  note?: string;
+}
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
