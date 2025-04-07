22fce11398a0ebbff3b90e2aba3fcc9179b5d8a5
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-07 23:02:14 +0700

    [IMP] check type,audit, transaction res

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://localhost:27017`, {
       user: 'root',
       pass: 'example',
-      dbName: 'accounting',
+      dbName: 'mikelopster',
+      authSource: 'admin',
     }),
+    TransactionModule,
   ],
   controllers: [],
   providers: [],
