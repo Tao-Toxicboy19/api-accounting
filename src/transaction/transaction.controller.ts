c40f1b66a0adfb4cf41121ae3c761f1f3fdfd3b8
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-08 08:26:15 +0700

    [IMP] create transaction

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { Transaction } from './schema/transaction.schema';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
+
+  @Post()
+  async create(@Body() dto: CreateTransactionDto): Promise<Transaction> {
+    return this.transactionService.create(dto);
+  }
 }
