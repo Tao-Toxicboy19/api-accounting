e695fa3eae78820cd654a5556d11d6013d867cc6
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-20 07:58:21 +0700

    [ADD] find users

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -2,13 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Transaction } from './schema/transaction.schema';
+import { FindTransactionDto } from './dto/find-transaction.dto';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
-  @Post()
+  @Post('create')
   async create(@Body() dto: CreateTransactionDto): Promise<Transaction> {
     return this.transactionService.create(dto);
+  }
+
+  @Post('by-user')
+  async findByUser(@Body() body: FindTransactionDto): Promise<Transaction[]> {
+    return this.transactionService.findByUser(body.userId);
   }
 }
