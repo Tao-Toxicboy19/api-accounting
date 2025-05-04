a12ef58a09e2fbd9099a7cd1ea229dda84953d9f
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-05-04 23:09:26 +0700

    [FIX] update up

--- src/transaction/dto/index.ts
+++ src/transaction/dto/index.ts
@@ -3,3 +3,4 @@ export * from './delete-transaction-by-user.dto';
 export * from './find-transaction-by-user.dto';
 export * from './update-transaction.dto';
 export * from './create-transaction-with-installment.dto';
+export * from './user-id.dto';
